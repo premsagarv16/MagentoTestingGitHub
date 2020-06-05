91d6ee29cb651de67ed0da204be61bb269dd18b3
PREM <PREM@DESKTOP-TT2OBQD.mshome.net>
2020-06-05 19:42:41 +0530

    second commit

--- MagentoMavenTest/src/test/java/Main.java
+++ MagentoMavenTest/src/test/java/Main.java
@@ -2,6 +2,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -12,7 +13,7 @@ public class Main {
 	WebDriver driver;
 	@BeforeMethod
 	public void open() {
-		driver=new ChromeDriver();
+		driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
